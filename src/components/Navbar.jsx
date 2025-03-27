94e0d75a4c422327c16807f8b3f2fe1c77d91021
Vikramop <veervikramsingh132@gmail.com>
2025-03-27 15:01:30 +0530

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+export function Navbar() {
+  return (
+    <div className="group relative  w-[98%] h-[10vh] top-6 left-1/2 transform -translate-x-1/2 border-2 border-transparent rounded-xl backdrop-blur-lg bg-white/40 transition-all duration-300">
+      <div className="absolute inset-0 rounded-md blur-lg border-2 border-transparent animate-borderColor"></div>
+      <div className="h-[100%] flex justify-between items-center px-4">
+        <div className="">LOGO</div>
+        <div>gg</div>
+      </div>
+    </div>
+  );
+}
